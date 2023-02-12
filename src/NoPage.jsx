b7ca7d476350282d94f6dc51dfe3c23254434935
SolindekDev <solindeklive.biznes@gmail.com>
2023-02-12 16:23:12 +0100

    Frontend: Really beautiful website with a cool looking navbar

---
+++ src/NoPage.jsx
@@ -0,0 +1,11 @@
+import * as Chakra from '@chakra-ui/react'
+
+function App() {
+  return (
+    <Chakra.Box p={1.5}>
+        <Chakra.Text>No page, you've got to the places where is nothing.. pretty sad :(</Chakra.Text>
+    </Chakra.Box>
+  )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { extendTheme, ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import * as ReactDOM from 'react-dom/client'
+import React from "react"
+
+import App from "./App"
+import NoPage from "./NoPage"
+
+const colors = {
+}
+
+const theme = extendTheme({ colors })
+
+const rootElement = document.getElementById('root')
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={ <App /> } />
+            <Route path="*" element={ <NoPage /> } />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
+  </React.StrictMode>
 )
