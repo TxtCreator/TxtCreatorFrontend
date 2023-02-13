71cacd047b619f33a30789a5c560c29d9fb41ec4
SolindekDev <solindeklive.biznes@gmail.com>
2023-02-13 12:08:44 +0100

    SEX

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,28 @@
 import { Link } from "react-router-dom";
 
-import Logo from "../../public/logo.png";
+import Logo from "../../public/logo22.png";
 
 function Navbar() {
   return (
     <header>
-      <div>
-        <Link to={"/"}>
-          <img src={Logo} alt="logo" width="84px" height="84px" />
-        </Link>
-      </div>
+            <div>
+              <Link to={"/"}>
+                <img src={Logo} alt="logo" width="82px" />
+              </Link>
+            </div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to={"/"}>Home</Link>
+                </li>
+                <li>
+                  <Link to={"/about"}>About</Link>
+                </li>
+                <li>
+                  <Link to={"/contact"}>Contact</Link>
+                </li>
+              </ul>
+            </nav>
     </header>
   )
 }
