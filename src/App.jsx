2be46fc0b135974d1183c02f2a0b0dfc32f1c80e
Nes0x <66802265+Nes0x@users.noreply.github.com>
2023-02-13 16:03:52 +0100

    fix scrolling and add About page

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,22 @@ import Creator from "./Pages/Creator.jsx";
 import React from "react";
 import Home from "./Pages/Home.jsx";
 import ComponentNavbar from "./Components/Navbar.jsx";
+import About from "./Pages/About.jsx";
 
 
 function App() {
   return (
-      <div>
+      <>
           <BrowserRouter>
               <ComponentNavbar />
               <Routes>
                   <Route path="/" element={ <Home /> }/>
                   <Route path="creator/:version" element={<Creator />} />
+                  <Route path="about" element={<About />} />
                   <Route path="*" element={ <NoPage /> } />
               </Routes>
           </BrowserRouter>
-      </div>
+      </>
   )
 }
 
