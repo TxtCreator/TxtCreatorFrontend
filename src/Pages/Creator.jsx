930d3e80d74bcdaaceee817298cdf5dea042e792
SolindekDev <solindeklive.biznes@gmail.com>
2023-02-14 19:25:13 +0100

    Styled full Creator.jsx

--- src/Pages/Creator.jsx
+++ src/Pages/Creator.jsx
@@ -19,6 +19,8 @@ function Creator() {
     const [minecraftPath, setMinecraftPath] = useState("");
     const [error, setError] = useState(null);
 
+    let texturesLength = 0;
+
     function addOrRemoveTexture(texture) {
         const textureType = texture.replace(/\/p\d*(.+?)/, "").replaceAll("9", "");
         if (txtModel.textures[texture] === undefined) {
@@ -29,10 +31,16 @@ function Creator() {
                 }
             }
             txtModel.textures[texture] = minecraftPath;
+            texturesLength++;
         } else {
             delete txtModel.textures[texture];
+            texturesLength--;
         }
         setTextures([...textures]);
+    }
+
+    function getTexturesLength() {
+        return texturesLength;
     }
 
     async function downloadTxt() {
@@ -65,20 +73,67 @@ function Creator() {
 
     return (
         <div className="creator">
-            {categories.map((category, index) => <img key={index} onClick={() => {
-                setSubCategories(category.subCategories);
-                setMinecraftPath(category.minecraftPath);
-            }} src={"/buttons/" + category.name + ".png"}/>)}
-            {subCategories.map((subCategory, index) => <p key={index} onClick={() => setTextures(subCategory.textures)}>{subCategory.name}</p>)}
-            {textures.map((texture, index) => {
-                if (txtModel.textures[texture] !== undefined) {
-                    return <img height="100" key={index} className="greenBorder" onClick={(event) => addOrRemoveTexture(texture, minecraftPath, event.target)} src={API_URL + "/" + texture} />
-                } else {
-                    return <img height="100" key={index} onClick={() => addOrRemoveTexture(texture)} src={API_URL + "/" + texture} />
-                }
-            } )}
-            {error !== null && <h1>{error}</h1>}
-            <a href="#" onClick={downloadTxt}>Pobierz TXT</a>
+            <div className="creatorLeftSide creatorLeft">
+                <div className="top">Grupy tekstur</div>
+                <div className="content">
+                    {
+                        categories.map((category, index) => (
+                            <div>
+                            <span onClick={() => {
+                                setSubCategories(category.subCategories);
+                                setMinecraftPath(category.minecraftPath);
+                            }} key={index}>
+                                {category.name[0].toUpperCase() + category.name.slice(1)}
+                            </span>
+                                <br />
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+            <div className="creatorRightSide creatorRight">
+                <div className="top">Pod grupy tekstur</div>
+                <div className="content">
+                    {
+                        subCategories.map((subCategory, index) => (
+                            (
+                                <div>
+                                <span key={index} onClick={() => setTextures(subCategory.textures)}>
+                                    {subCategory.name[0].toUpperCase() + subCategory.name.slice(1)}
+                                </span>
+                                    <br />
+                                </div>
+                            )
+                        ))
+                    }
+                </div>
+            </div>
+            <div className="creatorRightDownSide creatorRightDown">
+                <div className="top">Pod grupy tekstur</div>
+                <div className="content">
+                    {
+                        textures.map((texture, index) => {
+                            if (txtModel.textures[texture] !== undefined) {
+                                return <img height="100" key={index} className="greenBorder" onClick={(event) => addOrRemoveTexture(texture, minecraftPath, event.target)} src={API_URL + "/" + texture} />
+                            } else {
+                                return <img height="100" key={index} onClick={() => addOrRemoveTexture(texture)} src={API_URL + "/" + texture} />
+                            }
+                        })
+                    }
+                    {error !== null && <h1>{error}</h1>}
+                </div>
+
+            </div>
+            <div className="creatorFooterSide creatorFooter">
+                <div className="top">
+                    Informacje na temat twojej tekstury
+                </div>
+                <div className="content">
+                    <span className="creatorFooterText">
+                        <a href="#" onClick={downloadTxt}>Pobierz TXT</a>, który posiada w sobie { getTexturesLength() } tekstur.
+                    </span>
+                </div>
+            </div>
         </div>
     );
 }
