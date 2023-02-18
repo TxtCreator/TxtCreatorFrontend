f17f5846ec8c180c4c2067bf0f03f12908914c95
SolindekDev <solindeklive.biznes@gmail.com>
2023-02-18 18:11:43 +0100

    Added loader and scroll to loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+function Loader() {
+    return (
+        <div className="loader">
+            <div class="loadingio-spinner-rolling-ma394ftt1bl"><div class="ldio-miiu7u9rt1r">
+            <div></div>
+            </div></div>
+        </div>
+    )
+}
+
+export default Loader
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ComponentHomeContainer from '../components/HomeContainer';
 import ComponentHowItWork from '../components/HowItWork';
 import ComponentFooter from '../components/Footer';
+import ComponentLoader from '../components/Loader';
 
 function Home() {
     return (
