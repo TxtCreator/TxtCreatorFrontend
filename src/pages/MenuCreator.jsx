86fb02764be73b3a50298409d59c5752e6f48c3e
Nes0x <66802265+Nes0x@users.noreply.github.com>
2023-03-09 16:45:24 +0100

    add feature to display versions dynamically

--- src/pages/MenuCreator.jsx
+++ src/pages/MenuCreator.jsx
@@ -1,25 +1,44 @@
 import { Link } from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import Loader from "../components/Loader.jsx";
+
+const API_URL = "https://api.txtcreator.pl";
 
 function Creator() {
-    return (
-        <>
-            <div className="chooseVersion">
-                <div>
-                    <div className="chooseVersionTitle">
-                        <span>Wybierz wersję</span>
-                    </div>
-                    <div className="chooseVersionContainer">
-                        <div>
-                            <Link to={"/creator/1-8"}>1.8</Link>
-                        </div>
-                        <div>
-                            <Link to={"/creator/1-16"}>1.16</Link>
-                        </div>
-                    </div>
-                </div>
+    const [versions, setVersions] = useState([]);
+    const [isLoading, setLoading] = useState(true);
 
+    useEffect(() => {
+        async function getVersions() {
+            const data = (await axios.get(API_URL + "/txt/versions")).data;
+            setVersions(data);
+            setLoading(false);
+        }
+        getVersions();
+    }, []);
+
+    if (isLoading) {
+        return <Loader/>
+    }
+
+    return (
+        <div className="chooseVersion">
+            <div>
+                <div className="chooseVersionTitle">
+                    <span>Wybierz wersję</span>
+                </div>
+                <div className="chooseVersionContainer">
+                    {versions.map(version => {
+                        const formattedLink = "/creator/" + version.replace(".", "-");
+                        return <div>
+                            <Link to={formattedLink}>{version}</Link>
+                        </div>
+                    })}
+                </div>
             </div>
-        </>
+
+        </div>
     );
 }
 
